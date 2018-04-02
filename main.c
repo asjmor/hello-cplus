5b4dac4c5c2e868caea2c5c03d143c2c343d6fe0
libifeng <libifengs@outlook.com>
2018-04-02 08:06:18 +0800

    mac.0402

--- main.c
+++ main.c
@@ -1,5 +1,62 @@
-﻿#include "patest.h"
-#include "stdio.h"
+﻿//#include "patest.h"
+#include <stdio.h>
+#include <string.h>
+typedef struct
+{
+    char name[10];
+    char no[10];
+    double score;
+}Student;
+void input_1004()
+{
+    char tmp[30];
+
+    int n=1;
+    int i=0;
+    char input_n[10];
+    gets(input_n);
+    n= atoi(input_n);
+    if(n>10)
+        n=10;
+
+    Student stMax;
+    stMax.score=0;
+    Student stMin;
+    stMin.score=100;
+    Student stTmp;
+
+    while(1)
+    {
+        if(i>=n)
+            break;
+        gets(tmp);
+        char*p=strtok(tmp," ");
+        int j=0;
+        while (p!=NULL) {
+            if(j==0)
+                strncpy(stTmp.name,p,10);
+            if(j==1)
+                strncpy(stTmp.no,p,10);
+            if(j==2)
+                stTmp.score = atoi(p);
+
+            p=strtok(NULL," ");
+            ++j;
+        }
+        if(stMax.no=="")
+            stMax = stTmp;
+        if(stMin.no=="")
+            stMin = stTmp;
+        if(stTmp.score>stMax.score)
+            stMax=stTmp;
+        if(stTmp.score<stMin.score)
+            stMin = stTmp;
+
+        ++i;
+    }
+    printf("%s %s\n",stMax.name,stMax.no);
+    printf("%s %s\n",stMin.name,stMin.no);
+}
 
 int main(int argc, char *argv[])
 {
